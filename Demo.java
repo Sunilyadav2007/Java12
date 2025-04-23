44fb6594b80227fab3ba2d0915bd049e14b9181c
Sunilyadav2007 <sujangandoli@gmail.com>
2025-04-23 14:36:01 +0530

    this is add and substraction

---
+++ Demo.java
@@ -0,0 +1,15 @@
+public class Demo {
+    public static void main(String[] args) {
+        
+        int num1 = 10;
+        int num2 = 5;  
+
+        
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+
+        
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+    }
+}
